90cd59b68a3cb1cfb7b2c7dd061e85448c8d3ecb
DESKTOP-06IB626\alfre <alfredofweb@gmail.com>
2020-10-25 10:59:06 +0100

    fin CSS y comienzo modelo

---
+++ js/modelo.js
@@ -0,0 +1,18 @@
+const tareas = new Array();
+[
+    {
+        'idTarea': 1,
+        'titulo': 'estudiar Javascript',
+        'prioridad': 'urgente'
+    },
+    {
+        'idTarea': 2,
+        'titulo': 'Sacar al perro',
+        'prioridad': 'diaria'
+    },
+    {
+        'idTarea': 3,
+        'titulo': 'pedir pizza',
+        'prioridad': 'mensual'
+    }
+];
